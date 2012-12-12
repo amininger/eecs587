49ad98280d22e5779993399537d462e8f9f56852
Aaron Mininger <mininger@umich.edu>
2012-12-12 15:36:27 -0500

    added test results

--- long_test.sh
+++ long_test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #PBS -S /bin/sh
 #PBS -N AM-long
-#PBS -l feature=xeon5650,qos=flux,nodes=3:ppn=12,walltime=03:00:00,mem=8gb
+#PBS -l feature=xeon5650,qos=flux,nodes=3:ppn=12,walltime=12:00,mem=4gb
 #PBS -M mininger@umich.edu
 #PBS -q flux
 #PBS -A eecs587f12_flux
@@ -32,7 +32,7 @@ cd ~/eecs587/soar/out
 
 for use_var in 0 1
 do
-  for np in 3 4 6 10 18 34
+  for np in 3 4 6 10 18 34  
   do
     echo -e "\n\n-- NP($np) Use_Var($use_var) --"
     mpirun -np $np launch 10 $use_var ../../agents/long_test.soar
