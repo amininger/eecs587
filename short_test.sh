e586c16389f229e72c18fbf8b223bc9f17a3a9c4
Aaron Mininger <mininger@umich.edu>
2012-12-12 18:24:49 -0500

    added tests

---
+++ short_test.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#PBS -S /bin/sh
+#PBS -N AM-long
+#PBS -l feature=xeon5650,qos=flux,nodes=3:ppn=12,walltime=10:00,mem=4gb
+#PBS -M mininger@umich.edu
+#PBS -q flux
+#PBS -A eecs587f12_flux
+#PBS -V
+#
+
+echo "I ran on:"
+cat $PBS_NODEFILE
+
+source ~/eecs587/source_vars
+
+echo -e "\n\n=================="
+echo "original soar test"
+cd ~/eecs587/original_soar/out
+
+mpirun -np 1 launch ../../agents/qt_short.soar
+
+echo -e "\n\n=================="
+echo "solitary soar test"
+cd ~/eecs587/solitary_soar/out
+
+mpirun -np 1 launch ../../agents/qt_short.soar
+
+echo -e "\n\n================="
+echo "parallel soar test"
+
+cd ~/eecs587/soar/out
+
+for use_var in 0 1
+do
+  for np in 3 4 6 10 18 34  
+  do
+    echo -e "\n\n-- NP($np) Use_Var($use_var) --"
+    mpirun -np $np launch 10 $use_var ../../agents/qt_short.soar
+  done
+done
+
+
