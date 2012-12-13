e69cb7298005d9706f7f1cd57f69a6b4a4e0c488
Aaron Mininger <mininger@umich.edu>
2012-12-12 22:29:04 -0500

    moved folders

---
+++ data/read_scores.py
@@ -0,0 +1,42 @@
+STORING = 1
+RETRIEVING = 2
+
+def get_parens(line, num):
+	open = line.find("(") + 1
+	close = line.find(")") + 1
+	for i in range(1, num):
+		open = line.find("(", open) + 1
+		close = line.find(")", close) + 1
+	return line[open:close-1]
+
+def parseFile(fin, fout, case):
+	for i in range(0, 50000, 1000):
+		fout.write(str(i) + ", ")
+	for line in fin:
+		if(case == STORING and line.find("Storing") != -1):
+			time = get_parens(line, 1)
+			fout.write(time + ",")
+		elif(case == RETRIEVING and line.find("Query") != -1): 
+			time = line[12:len(line)-1]
+			fout.write(time + ",")
+		elif(line.find("-- NP") != -1):
+			fout.write("\n")
+			fout.write(get_parens(line, 1))
+			fout.write(" " + get_parens(line, 2) + ", ")
+		
+		
+
+fin = open("long_results1", 'r')
+fout = open("long.store.csv", "w")
+parseFile(fin, fout, STORING)
+
+fin.close()
+fout.close()
+
+fin = open("long_results1", "r")
+fout = open("long.retrieve.csv", "w")
+parseFile(fin, fout, RETRIEVING)
+
+fin.close()
+fout.close()
+
